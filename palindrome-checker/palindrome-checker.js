17a89cccd3716c0335df050f41ac9f3073d0f6dc
Pervez alam <pervezalam777@gmail.com>
2020-03-12 19:41:35 +0530

    One of the solution of palindrome code rectified

--- palindrome-checker/palindrome-checker.js
+++ palindrome-checker/palindrome-checker.js
@@ -2,7 +2,7 @@ function palindrome(str) {
   str = str.toLowerCase();
   str = str.replace(/[^a-z0-9]/gi, "");
   let len = str.length;
-  let mid = (len % 2 > 0) ? Math.floor(len/2)-1 : len/2;
+  let mid = Math.floor(len/2);
   for(let i = 0; i < mid; i++){
     if(str[i] !== str[len-(i+1)]){
       return false;
